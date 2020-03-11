c4f7108b21c930f47e985853b1b8e315723a0e83
Vladimir Popovich <vladimir.popovich@exyte.com>
2020-03-11 15:34:04 +0700

    GSTR-2 Move getFeatures into data.featuring

--- src/core/common.js
+++ src/core/common.js
@@ -4,6 +4,7 @@ const isValid = require('is-valid-path');
 const { parseAsync } = require('json2csv');
 const { createLogger, format, transports } = require('winston');
 const etherscan = require('./etherscan.api');
+const dataFeaturing = require('./data.featuring');
 
 const logger = createLogger({
     level: 'info',
@@ -226,71 +227,7 @@ const prepareTxsData = async function (options, txs, abis) {
 };
 
 const getFeatures = (data, input) => {
-    let features = [];
-    const re = /(\w+)(\[])/;
-    input.types.forEach((type, index) => {
-        let typeParts = type.split(re);
-
-        features.addUnique(`arg_${input.names[index]}`);
-        data[`arg_${input.names[index]}`] = input.inputs[index];
-        
-        //fixme
-        typeParts = typeParts.filter((el) => {
-            return el !== '';
-        });
-        
-        if (typeParts.length > 1 && typeParts[1] === '[]') {
-            features.addUnique(`arg_${input.names[index]}_length`);
-            data[`arg_${input.names[index]}_length`] = input.inputs[index].length;
-            if (typeParts[0].match('int') !== null) {
-                features.addUnique(`arg_${input.names[index]}_min`);
-                data[`arg_${input.names[index]}_min`] = Math.min(...input.inputs[index]);
-                features.addUnique(`arg_${data.names[index]}_max`);
-                data[`arg_${input.names[index]}_max`] = Math.max(...input.inputs[index]);
-            }
-            if (typeParts[0].match('string') !== null || typeParts[0].match('byte') !== null || typeParts[0].match('hex') !== null) {
-                const strLenArr = input.inputs[index].map((str) => {
-                    return str.length;
-                });
-                features.addUnique(`arg_${input.names[index]}_minLength`);
-                data[`arg_${input.names[index]}_minLength`] = Math.min(...strLenArr);
-                features.addUnique(`arg_${input.names[index]}_maxLength`);
-                data[`arg_${input.names[index]}_maxLength`] = Math.max(...strLenArr);
-                
-                let numArray = input.inputs[index].map((str) => {
-                    return isNaN ? false : Number(str);
-                });
-                
-                numArray = numArray.filter((el) => {
-                    return el;
-                });
-                
-                if (numArray.length) {
-                    features.addUnique(`arg_${input.names[index]}_min`);
-                    data[`arg_${input.names[index]}_min`] = Math.min(...numArray);
-                    features.addUnique(`arg_${input.names[index]}_max`);
-                    data[`arg_${input.names[index]}_max`] = Math.max(...numArray);
-                }
-            }
-            return;
-        }
-        
-        if (typeParts[0].match('byte') !== null) {
-            features.addUnique(`arg_${input.names[index]}_length`);
-            data[`arg_${input.names[index]}_length`] = input.inputs[index].length;
-        }
-        
-        if (typeParts[0].match('string') !== null) {
-            features.addUnique(`arg_${input.names[index]}_length`);
-            data[`arg_${input.names[index]}_length`] = input.inputs[index].length;
-            
-            if (input.inputs[index] && !isNaN(input.inputs[index])) {
-                features.addUnique(`arg_${input.names[index]}_num`);
-                data[`arg_${input.names[index]}_num`] = input.inputs[index];
-            }
-        }
-    });
-    return features;
+    return dataFeaturing.getFeatures(input, data);
 };
 
 const persistTxsData = async function (options, txs, features) {
---
+++ src/core/data.featuring.js
@@ -0,0 +1,61 @@
+
+function getFeatures(input, data) {
+    let features = [];
+    const re = /(\w+)(\[])/;
+    input.types.forEach((type, index) => {
+        let typeParts = type.split(re);
+        features.addUnique(`arg_${input.names[index]}`);
+        data[`arg_${input.names[index]}`] = input.inputs[index];
+        //fixme
+        typeParts = typeParts.filter((el) => {
+            return el !== '';
+        });
+        if (typeParts.length > 1 && typeParts[1] === '[]') {
+            features.addUnique(`arg_${input.names[index]}_length`);
+            data[`arg_${input.names[index]}_length`] = input.inputs[index].length;
+            if (typeParts[0].match('int') !== null) {
+                features.addUnique(`arg_${input.names[index]}_min`);
+                data[`arg_${input.names[index]}_min`] = Math.min(...input.inputs[index]);
+                features.addUnique(`arg_${data.names[index]}_max`);
+                data[`arg_${input.names[index]}_max`] = Math.max(...input.inputs[index]);
+            }
+            if (typeParts[0].match('string') !== null || typeParts[0].match('byte') !== null || typeParts[0].match('hex') !== null) {
+                const strLenArr = input.inputs[index].map((str) => {
+                    return str.length;
+                });
+                features.addUnique(`arg_${input.names[index]}_minLength`);
+                data[`arg_${input.names[index]}_minLength`] = Math.min(...strLenArr);
+                features.addUnique(`arg_${input.names[index]}_maxLength`);
+                data[`arg_${input.names[index]}_maxLength`] = Math.max(...strLenArr);
+                let numArray = input.inputs[index].map((str) => {
+                    return isNaN ? false : Number(str);
+                });
+                numArray = numArray.filter((el) => {
+                    return el;
+                });
+                if (numArray.length) {
+                    features.addUnique(`arg_${input.names[index]}_min`);
+                    data[`arg_${input.names[index]}_min`] = Math.min(...numArray);
+                    features.addUnique(`arg_${input.names[index]}_max`);
+                    data[`arg_${input.names[index]}_max`] = Math.max(...numArray);
+                }
+            }
+            return;
+        }
+        if (typeParts[0].match('byte') !== null) {
+            features.addUnique(`arg_${input.names[index]}_length`);
+            data[`arg_${input.names[index]}_length`] = input.inputs[index].length;
+        }
+        if (typeParts[0].match('string') !== null) {
+            features.addUnique(`arg_${input.names[index]}_length`);
+            data[`arg_${input.names[index]}_length`] = input.inputs[index].length;
+            if (input.inputs[index] && !isNaN(input.inputs[index])) {
+                features.addUnique(`arg_${input.names[index]}_num`);
+                data[`arg_${input.names[index]}_num`] = input.inputs[index];
+            }
+        }
+    });
+    return features;
+}
+
+module.exports = { getFeatures };

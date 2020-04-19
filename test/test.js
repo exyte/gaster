512fe10905fd87f807d287f63dd7d7f184397738
koteld <festbot@gmail.com>
2020-04-19 17:38:02 +0700

    add test for validateContractAddress

--- test/test.js
+++ test/test.js
@@ -1,14 +1,18 @@
-const main = require('../src/main');
+const main = require('../src/core/common');
 const { NetworkName } = require('../src/core/enums');
 const assert = require('assert');
 
-describe('main.js', function() {
-  describe('#validateContractAddress(options)', function() {
-    it('should return { validated: true, err: \'\' } with address=0x091bb13E6e55F07E9A42CBAa59e2c225C4a96E47 and ropsten=true', async function() {
-      let result = await main.validateContractAddress({
+describe('Gaster common', function() {
+  describe('#validateContractAddress', function() {
+    it('should return { validated: true, err: \'\' } with address=0xF324A8f3e0DbeD9059e5acBfC6C53a31A82b6AfB in Ropsten network', async function() {
+      const address = '0xF324A8f3e0DbeD9059e5acBfC6C53a31A82b6AfB';
+      const options = {
         net: NetworkName.ROPSTEN,
-        address: '0x091bb13E6e55F07E9A42CBAa59e2c225C4a96E47'
-      })
+      }
+      const result = await main.validateContractAddress(
+          address,
+          options,
+      );
 
       assert.equal(result.validated, true);
       assert.equal(result.err, '');
